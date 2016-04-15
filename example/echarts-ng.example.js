02ab7cc9869aba847f225cc7672b6d52fcb4427c
bornkiller <hjj491229492@hotmail.com>
2016-04-15 10:38:30 +0800

    添加饼图示例

--- example/echarts-ng.example.js
+++ example/echarts-ng.example.js
@@ -68,6 +68,42 @@
         }
       };
 
+      $scope.distribution = {
+        identity: $echarts.generateInstanceIdentity(),
+        config: {
+          tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+          },
+          series : [
+            {
+              name: '访问来源',
+              type: 'pie',
+              radius : '55%',
+              center: ['50%', '60%'],
+              label: {
+                normal: {
+                  show: true,
+                  formatter: "{b} : {c} ({d}%)"
+                }
+              },
+              data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+              ]
+            }
+          ]
+        }
+      };
+
       $timeout(function () {
         $scope.gauge.config.series[0].data = [{value: 75, name: '完成率'}];
         $echarts.updateEchartsInstance($scope.gauge.identity, $scope.gauge.config);
