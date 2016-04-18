0d94b1e8886e8c4651ebd7b1426764d53a14e890
bornkiller <hjj491229492@hotmail.com>
2016-04-18 15:33:07 +0800

    允许动态调整高度

--- example/src/echarts-ng-dimension.service.js
+++ example/src/echarts-ng-dimension.service.js
@@ -12,10 +12,28 @@
   function DimensionAssistanceProvider() {
     var ctx = this;
 
+    ctx.calculateDynamicDimension = function(series) {
+      var base = 45
+        , split = series.length
+        , length = series[0].data.length * split;
+
+      switch (true) {
+        case length < 5:
+          base = 60;
+          break;
+        case length >= 5 && length < 10:
+          base = 45;
+          break;
+        case length >= 10:
+          base = 35;
+          break;
+      }
+
+      return base * length + 'px';
+    };
     /**
      * @ngdoc service
      * @name echarts-ng.service:$dimension
-     *
      *
      * @description - echarts-ng dimension method
      */
@@ -25,6 +43,7 @@
       dimension.adaptEchartsDimension = adaptEchartsDimension;
       dimension.removeEchartsDimension = removeEchartsDimension;
       dimension.synchronizeEchartsDimension = synchronizeEchartsDimension;
+      dimension.adjustEchartsDimension = adjustEchartsDimension;
 
       return dimension;
 
@@ -86,6 +105,23 @@
        */
       function synchronizeEchartsDimension(instance) {
         instance.resize();
+      }
+
+      /**
+       * @ngdoc method
+       * @methodOf echarts-ng.service:$dimension
+       * @name echarts-ng.service:$dimension#adjustEchartsDimension
+       *
+       * @param {object} element - echarts instance container html element
+       * @param {array} series - standard echarts series
+       *
+       * @description - adjust echarts dimension dynamic
+       */
+      function adjustEchartsDimension(element, series) {
+
+        if (!angular.isArray(series) || !angular.isObject(series[0]) || !angular.isArray(series[0].data)) return;
+
+        element.style.height = ctx.calculateDynamicDimension(series);
       }
     }];
   }
