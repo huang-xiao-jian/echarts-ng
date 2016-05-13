4a2877491be2ad3191ad2cd3724c03e3776c1b29
bornkiller <hjj491229492@hotmail.com>
2016-05-13 17:55:34 +0800

    Test: 添加单元测试覆盖率报告

--- karma.conf.js
+++ karma.conf.js
@@ -34,13 +34,25 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'example/src/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'html' },
+        { type: 'lcov', subdir: 'lcov' }
+      ]
+    },
 
 
     // web server port
