54df966c0fff1b9e25382defadf18bbce6da34c3
bornkiller <hjj491229492@hotmail.com>
2016-05-16 19:13:30 +0800

    Example: waterfall, 更新瀑布图动态适配方式。

--- example/echarts-ng.waterfall.js
+++ example/echarts-ng.waterfall.js
@@ -23,14 +23,12 @@
         }
       };
 
-      $timeout(function() {
-        $echarts.updateEchartsInstance($scope.distribution.identity, {
-          series: [{
-            name: '生活费',
-            type: 'waterfall',
-            data: [400, 1000, 100, 400, 1100, 3000]
-          }]
-        })
+      $timeout(function () {
+        $scope.distribution.config.series[0] = {
+          name: '生活费',
+          type: 'waterfall',
+          data: [400, 1000, 100, 400, 1100, 3000]
+        };
       }, 1200);
     }])
 })(angular);
